947cdac69b520b89a65829aa26aef40a2ba2237a
DavidAkerlind <david.akerlind@icloud.com>
2026-03-17 14:30:16 +0100

    should work

--- part_1.s
+++ part_1.s
@@ -11,16 +11,15 @@ new_line: .asciiz "\n"
 	.include "print_array.s"
 	.include "merge_sort.s"
 	.include "merge.s"
-	.include "copy_element.s"
 	
 	
 main: 
 	subu $sp, $sp, 16 
 # Call search function (passing 2 arguments)	jal print_array
 	# Load vecor and size in to parameters
-	#la $a0, vector          # $a0 = base address of array
-	#lw $a1, size
-	#jal print_array
+	la $a0, vector          # $a0 = base address of array
+	lw $a1, size
+	jal print_array
 	
 # Merge sort call (int a[], int size) 
 	la $a0, vector          # $a0 = base address of array
